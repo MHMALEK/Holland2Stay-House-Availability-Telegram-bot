6a03147a13b11db51e4d2c39a5c106c61bfe0836
Mohammad Malek <mhos.malek@gmail.com>
2024-01-31 17:43:04 +0100

    update for each loop for users list

--- bot.py
+++ bot.py
@@ -50,76 +50,85 @@ async def daily_task(context: ContextTypes.DEFAULT_TYPE):
         # Get all users
         users = await fetch(f"{base_url}/users/list")
         # Send the result to all users
-        for chat_id in users["chat_ids"]:
-            # Send date message
-            today = datetime.now().strftime("%A, %d %B %Y")
-            message = await context.bot.send_message(
-                chat_id=chat_id,
-                text=f"Today is {today}. Loading latest update...",
-                read_timeout=60,
-                write_timeout=120,
-            )
+        for chat_id in users['chat_ids']:
+            try:
+                # Send date message
+                today = datetime.now().strftime("%A, %d %B %Y")
+                message = await context.bot.send_message(
+                    chat_id=chat_id,
+                    text=f"Today is {today}. Loading latest update...",
+                    read_timeout=60,
+                    write_timeout=120,
+                )
 
-            # Call api to get list of houses
-            cities_response = await fetch(f"{base_url}/h2s/list/all")
+                # Call api to get list of houses
+                cities_response = await fetch(f"{base_url}/h2s/list/all")
 
-            for city in cities_response["list"]:
-                # Check if results is a list
-                if isinstance(city["results"], list):
-                    for house in city["results"]:
-                        # Format message
-                        message_text = "*{}*\n{}\nPrice: {}\nAvailable Date: {}".format(
-                            city["city_name"],
-                            house["name"],
-                            house["price"],
-                            house["available_date"],
-                        )
+                for city in cities_response["list"]:
+                    # Check if results is a list
+                    if isinstance(city["results"], list):
+                        for house in city["results"]:
+                            # Format message
+                            message_text = (
+                                "*{}*\n{}\nPrice: {}\nAvailable Date: {}".format(
+                                    city["city_name"],
+                                    house["name"],
+                                    house["price"],
+                                    house["available_date"],
+                                )
+                            )
 
-                        # Create a link button
-                        keyboard = [
-                            [InlineKeyboardButton("More Info", url=house["url"])]
-                        ]
-                        reply_markup = InlineKeyboardMarkup(keyboard)
+                            # Create a link button
+                            keyboard = [
+                                [InlineKeyboardButton("More Info", url=house["url"])]
+                            ]
+                            reply_markup = InlineKeyboardMarkup(keyboard)
 
-                        # Send message
+                            # Send message
+                            await context.bot.send_message(
+                                chat_id=chat_id,
+                                text=message_text,
+                                parse_mode="Markdown",
+                                reply_markup=reply_markup,
+                                read_timeout=60,
+                                write_timeout=120,
+                            )
+
+                    else:
+                        # Send message with no house found
                         await context.bot.send_message(
                             chat_id=chat_id,
-                            text=message_text,
+                            text="*{}*\nNo house found".format(city["city_name"]),
                             parse_mode="Markdown",
-                            reply_markup=reply_markup,
-                            read_timeout=60,
-                            write_timeout=120,
                         )
 
-                else:
-                    # Send message with no house found
-                    await context.bot.send_message(
-                        chat_id=chat_id,
-                        text="*{}*\nNo house found".format(city["city_name"]),
-                        parse_mode="Markdown",
-                    )
+                # Edit loading message to say finished
+                await context.bot.edit_message_text(
+                    chat_id=chat_id,
+                    message_id=message.message_id,
+                    text=f"Today is {today}. Here is the latest update:",
+                    read_timeout=60,
+                    write_timeout=120,
+                )
+            except Forbidden as e:
+                print(f"The bot was blocked by the user with chat_id: {chat_id}")
 
-            # Edit loading message to say finished
-            await context.bot.edit_message_text(
-                chat_id=chat_id,
-                message_id=message.message_id,
-                text=f"Today is {today}. Here is the latest update:",
-                read_timeout=60,
-                write_timeout=120,
-            )
-    except Forbidden as e:
-        print(f"The bot was blocked by the user with chat_id: {chat_id}")
+                try:
+                    await fetch(f"{base_url}/users/{chat_id}", method="delete")
 
-        try:
-            await fetch(f"{base_url}/users/{chat_id}", method="delete")
+                except Exception as e:
+                    print(f"Request error: {e}")
 
-        except Exception as e:
-            print(f"Request error: {e}")
+            except Exception as e:
+                # Handle error
+                print(f"Error: {e}")
+                await context.bot.send_message(
+                    chat_id=chat_id, text=f"An error occurred: {e}"
+                )
 
     except Exception as e:
         # Handle error
         print(f"Error: {e}")
-        await context.bot.send_message(chat_id=chat_id, text=f"An error occurred: {e}")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -202,6 +211,7 @@ def create_and_start_bot():
 
         # schedule a job to run at 16 pm
         application.job_queue.run_daily(daily_task, time(hour=16, minute=0))
+        # application.job_queue.run_repeating(daily_task, interval=60, first=0)
 
         # start polling
         application.run_polling()
