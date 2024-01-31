e8883bdb5d582e39f18ba40cff9d7906f27a6105
Mohammad Malek <mhos.malek@gmail.com>
2024-01-31 15:53:56 +0100

    update timeout

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Bot
 from datetime import datetime
 from datetime import time
 import httpx
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 
 load_dotenv()
@@ -14,8 +15,12 @@ telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 base_url = os.getenv("HOUSE_REMINDER_BASE_URL")
 
 
+@retry(
+    stop=stop_after_attempt(3),  # stop after 3 attempts
+    wait=wait_fixed(5),  # wait 5 seconds between attempts
+)
 async def fetch(url, method="get", data=None):
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=None) as client:
         if method == "get":
             response = await client.get(url)
         elif method == "post":
@@ -37,19 +42,26 @@ class UserNotFoundError(Exception):
 
 
 async def daily_task(context: ContextTypes.DEFAULT_TYPE):
+    print("asdsadasd")
     try:
         # Get all users
         users = await fetch(f"{base_url}/users/list")
         # Send the result to all users
+        # for chat_id in users["chat_ids"]:
         for chat_id in users["chat_ids"]:
             # Send date message
             today = datetime.now().strftime("%A, %d %B %Y")
             message = await context.bot.send_message(
-                chat_id=chat_id, text=f"Today is {today}. Loading latest update..."
+                chat_id=chat_id,
+                text=f"Today is {today}. Loading latest update...",
+                read_timeout=60,
+                write_timeout=120,
             )
 
             # Call api to get list of houses
             cities_response = await fetch(f"{base_url}/h2s/list/all")
+
+            print("cities_response", len(cities_response.keys()))
 
             for city in cities_response["list"]:
                 # Check if results is a list
@@ -75,6 +87,8 @@ async def daily_task(context: ContextTypes.DEFAULT_TYPE):
                             text=message_text,
                             parse_mode="Markdown",
                             reply_markup=reply_markup,
+                            read_timeout=60,
+                            write_timeout=120,
                         )
 
                 else:
@@ -90,9 +104,17 @@ async def daily_task(context: ContextTypes.DEFAULT_TYPE):
                 chat_id=chat_id,
                 message_id=message.message_id,
                 text=f"Today is {today}. Here is the latest update:",
+                read_timeout=60,
+                write_timeout=120,
             )
     except Forbidden as e:
         print(f"The bot was blocked by the user with chat_id: {chat_id}")
+
+        try:
+            await fetch(f"{base_url}/users/{chat_id}", method="delete")
+
+        except Exception as e:
+            print(f"Request error: {e}")
 
     except Exception as e:
         # Handle error
@@ -104,7 +126,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     await context.bot.send_message(
         chat_id=chat_id,
-        text="Welcome! You can turn on reminders with /set_reminder command. We will send you about all available houses every day on this bot",
+        text="asd! You can turn on reminders with /set_reminder command. We will send you about all available houses every day on this bot",
     )
 
 
@@ -178,8 +200,9 @@ def create_and_start_bot():
         application.add_handler(set_reminder_handler)
         application.add_handler(unset_reminder_handler)
 
-        # schedule a job to run at 9 am
-        application.job_queue.run_daily(daily_task, time(hour=12, minute=0))
+        # schedule a job to run at 16 pm
+        application.job_queue.run_daily(daily_task, time(hour=16, minute=0))
+        # application.job_queue.run_repeating(daily_task, interval=60, first=0)
 
         # start polling
         application.run_polling()
